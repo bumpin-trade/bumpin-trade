eb372fa172929be1e12c97a544d866f9d8f37e52
jax <>
2024-06-25 23:44:38 +0800

    pool相关方法提交

--- sdk/src/pool.ts
+++ sdk/src/pool.ts
@@ -1,17 +1,53 @@
 import {OracleClient} from "./oracles/types";
 import {PublicKey} from '@solana/web3.js';
-import {AccountSubscriber} from "./account/types";
-import {State, UserAccount} from "./types";
+import {AccountSubscriber, DataAndSlot} from "./account/types";
+import {Pool, State, UserAccount} from "./types";
 import {BumpinClientConfig} from "./bumpinClientConfig";
 import {PollingPoolAccountSubscriber} from "./account/pollingPoolAccountSubscriber";
+import {BulkAccountLoader} from "./account/bulkAccountLoader";
+import {AnchorProvider, BN, Program, Wallet} from "@coral-xyz/anchor";
+import {BumpinUtils} from "./utils/utils";
+import {BumpinTrade} from "./types/bumpin_trade";
 
-export class Pool {
-    // oracleClient: OracleClient
-    // poolAccountSubscriber: AccountSubscriber<Pool>
-    // state: State
-    //
-    // constructor(poolPublicKey: PublicKey, clientConfig: BumpinClientConfig) {
-    //     this.oracleClient = clientConfig.oracleClient;
-    //     this.poolAccountSubscriber = new PollingPoolAccountSubscriber(clientConfig.program, poolPublicKey, clientConfig.bulkAccountLoader);
-    // }
+export class PoolComponent {
+    oracleClient: OracleClient
+    state: State
+    program: Program<BumpinTrade>;
+    pools: Map<PublicKey, PollingPoolAccountSubscriber>;
+
+    constructor(oracleClient: OracleClient, bulkAccountLoader: BulkAccountLoader, program: Program<BumpinTrade>) {
+        this.oracleClient = oracleClient;
+        this.program = program;
+        for (let i = 0; i < this.state!.numberOfPools; i++) {
+            const [pda, _] = BumpinUtils.getPoolPda(this.program, i);
+            let poolAccountSubscriber = new PollingPoolAccountSubscriber(program, pda, bulkAccountLoader);
+            this.pools.set(pda, poolAccountSubscriber);
+        }
+    }
+
+    public async subscribe() {
+        for (let poolAccountSubscriber of this.pools.values()) {
+            await poolAccountSubscriber.subscribe();
+        }
+    }
+
+    public getPool(poolKey: PublicKey): Pool {
+        const poolAccountSubscriber: PollingPoolAccountSubscriber | undefined = this.pools.get(poolKey);
+        if (poolAccountSubscriber === undefined) {
+            throw new Error(`Pool with the key ${poolKey} does not exist`);
+        }
+        return poolAccountSubscriber.getUserAccountAndSlot().data;
+    }
+
+
+    public getPoolAvailableLiquidity(poolKey: PublicKey): BN {
+        let pool = this.getPool(poolKey);
+    }
+
+    public async getPoolUsd(poolKey: PublicKey): Promise<BN> {
+        let pool = this.getPool(poolKey);
+        let oraclePriceData = await this.oracleClient.getOraclePriceData(pool.poolMint);
+
+        pool.poolBalance.amount.add(pool.poolBalance.unSettleAmount).multiple(oraclePriceData.price);
+    }
 }
