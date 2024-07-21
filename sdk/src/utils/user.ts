8e8deb02023e644331dfa0cbd47975bf490edbd7
Johnny Wang <52704476+JohnnnyWang@users.noreply.github.com>
2024-07-21 16:18:20 +0800

    update

--- sdk/src/utils/user.ts
+++ sdk/src/utils/user.ts
@@ -1,39 +1,51 @@
 import {
-  OrderStatus,
-  PositionStatus,
-  UserAccount,
-  UserOrder,
-  UserPosition,
-  UserStake,
-  UserStakeStatus,
-  UserToken,
-  UserTokenStatus,
+    OrderStatus,
+    PositionStatus,
+    UserAccount,
+    UserOrder,
+    UserPosition,
+    UserStake,
+    UserStakeStatus,
+    UserToken,
+    UserTokenStatus,
 } from "../typedef";
 // @ts-ignore
-import { isEqual } from "lodash";
+import {isEqual} from "lodash";
 
 export class BumpinUserUtils {
-  public static getMyStake(me: UserAccount): UserStake[] {
-    return me.stakes.filter((stake) => {
-      return isEqual(stake.userStakeStatus, UserStakeStatus.USING);
-    });
-  }
+    public static getMyStake(me: UserAccount): UserStake[] {
+        return me.stakes.filter((stake) => {
+            return isEqual(stake.userStakeStatus, UserStakeStatus.USING);
+        });
+    }
 
-  public static getMyToken(me: UserAccount): UserToken[] {
-    return me.tokens.filter((token) => {
-      return isEqual(token.userTokenStatus, UserTokenStatus.USING);
-    });
-  }
+    public static getMyToken(me: UserAccount): UserToken[] {
+        return me.tokens.filter((token) => {
+            return isEqual(token.userTokenStatus, UserTokenStatus.USING);
+        });
+    }
 
-  public static getMyPosition(me: UserAccount): UserPosition[] {
-    return me.positions.filter((position) => {
-      return isEqual(position.status, PositionStatus.USING);
-    });
-  }
+    public static getMyPosition(me: UserAccount, isPortfolioMargin?: boolean): UserPosition[] {
+        return me.positions.filter((position) => {
+            if (!isEqual(position.status, PositionStatus.USING)) {
+                return false;
+            }
+            if (isPortfolioMargin) {
+                return position.isPortfolioMargin === isPortfolioMargin;
+            }
+            return true;
+        });
+    }
 
-  public static getMyOrder(me: UserAccount): UserOrder[] {
-    return me.orders.filter((order) => {
-      return isEqual(order.status, OrderStatus.USING);
-    });
-  }
+    public static getMyOrder(me: UserAccount, isPortfolioMargin?: boolean): UserOrder[] {
+        return me.orders.filter((order) => {
+            if (!isEqual(order.status, OrderStatus.USING)) {
+                return false;
+            }
+            if (isPortfolioMargin) {
+                return order.isPortfolioMargin === isPortfolioMargin;
+            }
+            return true;
+        });
+    }
 }
