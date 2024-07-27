ed3fd9a7d5981d3db4ad740bdb74f4418b6b855e
jax <>
2024-07-27 15:01:26 +0800

    精度修改！

--- sdk/src/consts.ts
+++ sdk/src/consts.ts
@@ -1,4 +1,4 @@
-import { BN } from '@coral-xyz/anchor';
+import {BN} from '@coral-xyz/anchor';
 
 export class C {
     public static readonly RATE_MULTIPLIER_BN: BN = new BN(100000);
@@ -7,6 +7,8 @@ export class C {
     public static readonly RATE_MULTIPLIER_NUMBER: number =
         10 ** C.RATE_MULTIPLIER_EXPONENT;
     public static readonly SMALL_RATE_MULTIPLIER_NUMBER: number = 10;
+    public static readonly FUNDING_OR_BORROWING_RATE_MULTIPLIER_NUMBER: number = 18;
+
 
     public static readonly PRICE_EXPONENT_BN: BN = new BN(8);
     public static readonly PRICE_EXPONENT: number = 100000000;
