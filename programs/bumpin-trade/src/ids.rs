1e125821d120b74acdd9457cd2f57947864feebe
Johnny Wang <52704476+JohnnnyWang@users.noreply.github.com>
2024-10-21 22:44:44 +0800

    update

--- programs/bumpin-trade/src/ids.rs
+++ programs/bumpin-trade/src/ids.rs
@@ -1,4 +1,8 @@
 pub mod pyth_program {
     use anchor_lang::prelude::*;
+
+    #[cfg(feature = "localnet")]
     declare_id!("FsJ3A3u2vn5cTVofAjvy6y5kwABJAqYWpe4975bi2epH");
+    #[cfg(not(feature = "localnet"))]
+    declare_id!("rec5EKMGg6MxZYaMdyBfgwp4d5rB9T1VQH5pJv5LtFJ");
 }
