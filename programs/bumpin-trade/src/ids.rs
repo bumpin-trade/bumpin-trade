c02956dda9bcda182a51c93983d92f4b43b698a6
Johnny Wang <52704476+JohnnnyWang@users.noreply.github.com>
2024-10-21 22:38:41 +0800

    update

--- programs/bumpin-trade/src/ids.rs
+++ programs/bumpin-trade/src/ids.rs
@@ -1,11 +1,4 @@
 pub mod pyth_program {
     use anchor_lang::prelude::*;
-    #[cfg(feature = "mainnet-beta")]
     declare_id!("FsJ3A3u2vn5cTVofAjvy6y5kwABJAqYWpe4975bi2epH");
-    #[cfg(feature = "testnet")]
-    declare_id!("8tfDNiaEyrV6Q1U4DEXrEigs9DoDtkugzFbybENEbCDz");
-    #[cfg(feature = "devnet")]
-    declare_id!("gSbePebfvPy7tRqimPoVecS2UsBvYv46ynrzWocc92s");
-    #[cfg(not(feature = "mainnet-beta"))]
-    declare_id!("AQkVcL5spcyrqiKNJykGWGD78ry8Erkuub2t2ogUVWca");
 }
